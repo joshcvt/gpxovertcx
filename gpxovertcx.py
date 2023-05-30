98e7160f9971c3ea650450dd4834a3c29002ebe7
Josh Crockett <josh.crockett@gmail.com>
2023-05-30 00:03:39 -0400

    Something wrong with ElementTree.

--- gpxovertcx.py
+++ gpxovertcx.py
@@ -14,10 +14,16 @@ the GPX track on the TCX file by adding latitude, longitude, and elevation infor
 Result:
 gpxpy doesn't like the GPX from Mapometer.com.
 
+New prompt:
+
+The logic is good, but the gpxpy module doesn't like my gpx file. ElementTree believes it's valid XML. Can 
+you use this same logic, but use xml.etree.ElementTree to parse the GPX instead of gpxpy?
+
+Result:
+with some debug statements (not captured), it's finding zero elements of trkpt or Trackpoint.
 """
 
 import xml.etree.ElementTree as ET
-from gpxpy import parse
 
 def overlay_gpx_on_tcx(tcx_file, gpx_file, output_file):
     # Load TCX file
@@ -25,12 +31,14 @@ def overlay_gpx_on_tcx(tcx_file, gpx_file, output_file):
     tcx_root = tcx_tree.getroot()
 
     # Load GPX file
-    gpx_data = parse(gpx_file)
-    gpx_track = gpx_data.tracks[0]
-    gpx_segments = gpx_track.segments
+    gpx_tree = ET.parse(gpx_file)
+    gpx_root = gpx_tree.getroot()
+
+    # Find all trackpoints in the GPX file
+    trackpoints = gpx_root.findall('.//trkpt')
 
     # Iterate over TCX trackpoints
-    for trackpoint in tcx_root.findall('.//Trackpoint'):
+    for i, trackpoint in enumerate(tcx_root.findall('.//Trackpoint')):
         # Get time from TCX trackpoint (if available)
         time_element = trackpoint.find('.//Time')
         time = time_element.text if time_element is not None else ''
@@ -40,31 +48,23 @@ def overlay_gpx_on_tcx(tcx_file, gpx_file, output_file):
         distance = float(distance_element.text) if distance_element is not None else None
 
         if distance is not None:
-            # Find the corresponding GPX trackpoint based on distance
-            for segment in gpx_segments:
-                for i in range(len(segment.points) - 1):
-                    point1 = segment.points[i]
-                    point2 = segment.points[i + 1]
-                    segment_distance = point2.distance_2d(point1)
+            # Find the corresponding GPX trackpoint based on index
+            if i < len(trackpoints):
+                gpx_trackpoint = trackpoints[i]
 
-                    if distance <= segment_distance:
-                        # Interpolate latitude, longitude, and elevation
-                        segment_ratio = distance / segment_distance
-                        lat = point1.latitude + (point2.latitude - point1.latitude) * segment_ratio
-                        lon = point1.longitude + (point2.longitude - point1.longitude) * segment_ratio
-                        elevation = point1.elevation + (point2.elevation - point1.elevation) * segment_ratio
+                # Extract latitude, longitude, and elevation from GPX trackpoint
+                lat = float(gpx_trackpoint.attrib['lat'])
+                lon = float(gpx_trackpoint.attrib['lon'])
+                elevation_element = gpx_trackpoint.find('ele')
+                elevation = float(elevation_element.text) if elevation_element is not None else None
 
-                        # Add latitude, longitude, and elevation to TCX trackpoint
-                        lat_element = ET.SubElement(trackpoint, 'Position/LatitudeDegrees')
-                        lat_element.text = str(lat)
-                        lon_element = ET.SubElement(trackpoint, 'Position/LongitudeDegrees')
-                        lon_element.text = str(lon)
-                        elevation_element = ET.SubElement(trackpoint, 'AltitudeMeters')
-                        elevation_element.text = str(elevation)
-
-                        break  # Exit the loop once the GPX trackpoint is found and processed
-                    else:
-                        distance -= segment_distance
+                # Add latitude, longitude, and elevation to TCX trackpoint
+                lat_element = ET.SubElement(trackpoint, 'Position/LatitudeDegrees')
+                lat_element.text = str(lat)
+                lon_element = ET.SubElement(trackpoint, 'Position/LongitudeDegrees')
+                lon_element.text = str(lon)
+                elevation_element = ET.SubElement(trackpoint, 'AltitudeMeters')
+                elevation_element.text = str(elevation)
 
     # Save the modified TCX file
     tcx_tree.write(output_file)
