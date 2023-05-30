063591c874bebf50aedb5467ac3dd5ac0d73fb1b
Josh Crockett <josh.crockett@gmail.com>
2023-05-30 00:30:03 -0400

    tries to interpolate better, fails.

--- gpxovertcx.py
+++ gpxovertcx.py
@@ -39,6 +39,13 @@ Result:
 
 It doesn't have the distances right, but it produces workable XML that imports to Smashrun.
 
+New prompt:
+
+This code mapped a 5010-meter course to only the first 1565 meters of the TCX inertial distance. Are you sure 
+you're interpolating correctly?
+
+Result: tries to do haversine, instead just moves us to the farthest away point of the course.
+
 """
 
 from xml.dom import minidom
@@ -55,6 +62,18 @@ def overlay_gpx_on_tcx(tcx_file, gpx_file, output_file):
     # Find all trackpoints in the GPX file
     trackpoints = gpx_root.getElementsByTagName('trkpt')
 
+    # Calculate cumulative distances in the GPX track
+    cumulative_distances = [0.0]
+    for i in range(1, len(trackpoints)):
+        lat1 = float(trackpoints[i-1].getAttribute('lat'))
+        lon1 = float(trackpoints[i-1].getAttribute('lon'))
+        lat2 = float(trackpoints[i].getAttribute('lat'))
+        lon2 = float(trackpoints[i].getAttribute('lon'))
+
+        distance = haversine_distance(lat1, lon1, lat2, lon2)
+        cumulative_distance = cumulative_distances[i-1] + distance
+        cumulative_distances.append(cumulative_distance)
+
     # Iterate over TCX trackpoints
     for i, trackpoint in enumerate(tcx_root.getElementsByTagName('Trackpoint')):
         # Get time from TCX trackpoint (if available)
@@ -66,50 +85,86 @@ def overlay_gpx_on_tcx(tcx_file, gpx_file, output_file):
         distance = float(distance_elements[0].firstChild.nodeValue) if distance_elements else None
 
         if distance is not None:
-            # Find the corresponding GPX trackpoint based on index
-            if i < len(trackpoints):
-                gpx_trackpoint = trackpoints[i]
+            # Find the corresponding GPX trackpoints based on distance
+            for j in range(1, len(cumulative_distances)):
+                if distance <= cumulative_distances[j]:
+                    # Interpolate latitude, longitude, and elevation
+                    ratio = (distance - cumulative_distances[j-1]) / (cumulative_distances[j] - cumulative_distances[j-1])
+                    prev_trackpoint = trackpoints[j-1]
+                    next_trackpoint = trackpoints[j]
 
-                # Extract latitude, longitude, and elevation from GPX trackpoint
-                lat = float(gpx_trackpoint.getAttribute('lat'))
-                lon = float(gpx_trackpoint.getAttribute('lon'))
-                elevation_elements = gpx_trackpoint.getElementsByTagName('ele')
-                elevation = float(elevation_elements[0].firstChild.nodeValue) if elevation_elements else None
+                    lat1 = float(prev_trackpoint.getAttribute('lat'))
+                    lon1 = float(prev_trackpoint.getAttribute('lon'))
+                    lat2 = float(next_trackpoint.getAttribute('lat'))
+                    lon2 = float(next_trackpoint.getAttribute('lon'))
 
-                # Create latitude, longitude, and elevation elements
-                lat_element = tcx_doc.createElement('LatitudeDegrees')
-                lat_text = tcx_doc.createTextNode(str(lat))
-                lat_element.appendChild(lat_text)
+                    lat = lat1 + (lat2 - lat1) * ratio
+                    lon = lon1 + (lon2 - lon1) * ratio
 
-                lon_element = tcx_doc.createElement('LongitudeDegrees')
-                lon_text = tcx_doc.createTextNode(str(lon))
-                lon_element.appendChild(lon_text)
+                    elevation_elements = next_trackpoint.getElementsByTagName('ele')
+                    if elevation_elements:
+                        elevation1 = float(prev_trackpoint.getElementsByTagName('ele')[0].firstChild.nodeValue)
+                        elevation2 = float(elevation_elements[0].firstChild.nodeValue)
+                        elevation = elevation1 + (elevation2 - elevation1) * ratio
+                    else:
+                        elevation = None
 
-                elevation_element = tcx_doc.createElement('AltitudeMeters')
-                elevation_text = tcx_doc.createTextNode(str(elevation))
-                elevation_element.appendChild(elevation_text)
+                    # Create latitude, longitude, and elevation elements
+                    lat_element = tcx_doc.createElement('LatitudeDegrees')
+                    lat_text = tcx_doc.createTextNode(str(lat))
+                    lat_element.appendChild(lat_text)
 
-                # Create position element
-                position_element = tcx_doc.createElement('Position')
-                position_element.appendChild(lat_element)
-                position_element.appendChild(lon_element)
+                    lon_element = tcx_doc.createElement('LongitudeDegrees')
+                    lon_text = tcx_doc.createTextNode(str(lon))
+                    lon_element.appendChild(lon_text)
 
-                # Add position and elevation to TCX trackpoint
-                trackpoint.appendChild(position_element)
-                trackpoint.appendChild(elevation_element)
+                    if elevation is not None:
+                        elevation_element = tcx_doc.createElement('AltitudeMeters')
+                        elevation_text = tcx_doc.createTextNode(str(elevation))
+                        elevation_element.appendChild(elevation_text)
 
-            else:
-                # Create empty position and elevation elements
-                position_element = tcx_doc.createElement('Position')
-                elevation_element = tcx_doc.createElement('AltitudeMeters')
+                    # Remove existing position and elevation elements from TCX trackpoint
+                    position_elements = trackpoint.getElementsByTagName('Position')
+                    for position_element in position_elements:
+                        trackpoint.removeChild(position_element)
+                    elevation_elements = trackpoint.getElementsByTagName('AltitudeMeters')
+                    for elevation_element in elevation_elements:
+                        trackpoint.removeChild(elevation_element)
 
-                # Add empty position and elevation to TCX trackpoint
-                trackpoint.appendChild(position_element)
-                trackpoint.appendChild(elevation_element)
+                    # Add interpolated latitude, longitude, and elevation to TCX trackpoint
+                    position_element = tcx_doc.createElement('Position')
+                    position_element.appendChild(lat_element)
+                    position_element.appendChild(lon_element)
+                    trackpoint.appendChild(position_element)
+                    if elevation is not None:
+                        trackpoint.appendChild(elevation_element)
+
+                    break
 
     # Save the modified TCX file
     with open(output_file, 'w') as f:
         f.write(tcx_doc.toprettyxml(indent='\t'))
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    # Calculate the distance between two coordinates using the Haversine formula
+    # Reference: https://en.wikipedia.org/wiki/Haversine_formula
+    from math import radians, sin, cos, sqrt, atan2
+
+    # Convert latitude and longitude to radians
+    lat1_rad = radians(lat1)
+    lon1_rad = radians(lon1)
+    lat2_rad = radians(lat2)
+    lon2_rad = radians(lon2)
+
+    # Haversine formula
+    dlon = lon2_rad - lon1_rad
+    dlat = lat2_rad - lat1_rad
+    a = sin(dlat / 2) ** 2 + cos(lat1_rad) * cos(lat2_rad) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    radius = 6371  # Radius of the Earth in kilometers
+    distance = radius * c
+
+    return distance
 
 # Usage example
 tcx_file = 'source.tcx'
